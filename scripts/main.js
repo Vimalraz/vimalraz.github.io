df10000c6f33610f6f39cedba5f9d15d76d8353e
Vimalraz <vimalraj.murugesan@gmail.com>
2020-10-20 00:18:56 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -20,7 +20,7 @@ function setUserName() {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    myHeading.innerHTML = 'This is a learning test site,' + myName;
   }
 }
 
