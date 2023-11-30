f9e501924a7b02a09c014a52306c5c7d2ccfda83
Vimalraj <vimalraj.murugesan@gmail.com>
2023-11-30 22:51:11 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,39 @@
-let myImage = document.querySelector('img');
+// Load blog content from a JSON file
+fetch('blogs.json')
+    .then(response => response.json())
+    .then(data => {
+        const blogSection = document.getElementById('blog');
 
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute('src','images/firefox2.png');
-    } else {
-      myImage.setAttribute('src','images/firefox-icon.png');
+        data.forEach(blog => {
+            const blogPost = document.createElement('article');
+            blogPost.innerHTML = `
+                <h2>${blog.title}</h2>
+                <p>${blog.content}</p>
+            `;
+            blogSection.appendChild(blogPost);
+        });
+    })
+    .catch(error => console.error('Error fetching blogs:', error));
+
+// Display liked designs as links or images
+const likedDesignsSection = document.getElementById('liked-designs');
+
+// Example content (replace this with your liked designs data)
+const likedDesigns = [
+    { type: 'link', content: 'Link 1', href: 'https://example.com/link1' },
+    { type: 'image', content: 'Image 1', src: 'path/to/image1.jpg' },
+    // Add more liked designs here...
+];
+
+likedDesigns.forEach(design => {
+    if (design.type === 'link') {
+        const link = document.createElement('a');
+        link.textContent = design.content;
+        link.href = design.href;
+        likedDesignsSection.appendChild(link);
+    } else if (design.type === 'image') {
+        const image = document.createElement('img');
+        image.src = design.src;
+        likedDesignsSection.appendChild(image);
     }
-}
-let myButton = document.querySelector('button');
-
-myButton.onclick = function() {
-  setUserName();
-}
-
-function setUserName() {
-  let myName = prompt('Please enter your name.');
-  if(!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'This is a learning test site,' + myName;
-  }
-}
-
-let myHeading = document.querySelector('h1');
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
-}
+});
